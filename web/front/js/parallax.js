a3967ac28d5a6e91cddfb382eee05a514c8423e2
LeoHugues <leo.hugues@hotmail.fr>
2017-03-11 17:59:18 +0100

    mise en place du js pour gerer les deux parallax

--- web/front/js/parallax.js
+++ web/front/js/parallax.js
@@ -1,39 +1,25 @@
 function castParallax() {
 
-  var opThresh = 350;
-  var opFactor = 750;
+  var parallaxContainers = $('.parallax-container');
 
-  /*
-   $(window).scroll(function(){
-   var windowScroll = $(window).scrollTop();
-
-   $('.keyart_layer.parallax').each(function(){
-   var $layer = $(this);
-   var yPos = -(windowScroll * $layer.data('speed') / 100);
-   $layer.css({
-   "transform" : "translate3d(0px, " + yPos + "px, 0px)"
-   });
-
-   });
-
-
-   var backgroundOpacity = (windowScroll > opThresh ? (windowScroll - opThresh) / opFactor : 0);
-   $('#keyart-scrim').css('opacity', backgroundOpacity);
-   });
-
-   */
   window.addEventListener("scroll", function(event){
 
-    var top = this.pageYOffset;
+    for (var j = 0; j < parallaxContainers.length; j++) {
+      var topParallaxContainer = this.pageYOffset - parallaxContainers[j].offsetTop;
 
-    var layers = document.getElementsByClassName("parallax");
-    var layer, speed, yPos;
-    for (var i = 0; i < layers.length; i++) {
-      layer = layers[i];
-      speed = layer.getAttribute('data-speed');
-      var yPos = -(top * speed / 100);
-      layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
-
+      var heightContainer = parallaxContainers[j].offsetHeight;
+      if (topParallaxContainer >= 0 && topParallaxContainer < heightContainer) {
+        var layers = document.getElementsByClassName("parallax".concat((j+1).toString()));
+        var layer, speed, yPos;
+        for (var i = 0; i < layers.length; i++) {
+          layer = layers[i];
+          speed = layer.getAttribute('data-speed');
+          yPos = -(topParallaxContainer * speed / 100);
+          layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+          layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        }
+        parallaxContainers[j].style.transform = 'translate3d(0px, ' + heightContainer - topParallaxContainer + 'px, 0px)';
+      }
     }
   });
 
