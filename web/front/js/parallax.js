188df218fcd17fb663058355a34efc6e3a91683f
Leo HUGUES <lehug@p-mpl-raoh.dhcp.mpl.intranet>
2017-03-20 18:09:16 +0100

    reglage des ancres

--- web/front/js/parallax.js
+++ web/front/js/parallax.js
@@ -53,11 +53,34 @@ function castSmoothScroll() {
     speed: 300,
     ease: 'linear'
   });
+
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 100
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 }
 
 function startSite() {
   var platform = navigator.platform.toLowerCase();
-  var userAgent = navigator.userAgent.toLowerCase();
 
   if ( platform.indexOf('ipad') != -1  ||  platform.indexOf('iphone') != -1 )
   {
